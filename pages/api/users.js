837c8d13eef837202b3800e89fd506d23ff3a708
Gauthier Bosson <bosson.gauthier27@gmail.com>
2020-09-16 12:35:58 +0200

    connection to aws

--- pages/api/users.js
+++ pages/api/users.js
@@ -2,10 +2,10 @@ import db from "../../lib/db";
 const escape = require('sql-template-strings');
 
 export default async (req, res) => {
-	const users = db.query(escape`
+	const users = await db.query(escape`
 		SELECT *
 		FROM USER
 	`)
 
-	res.status(200).json({ users });
+	res.status(200).json(users);
 }
--- pages/users.js
+++ pages/users.js
@@ -1,3 +1,22 @@
+import useSWR from "swr";
+
 export default function Users() {
-  return <h1>USERS</h1>;
+  const { data, error } = useSWR("/api/users");
+
+  if (error) return <p>fail</p>;
+
+  if (!data) return <p>Loading</p>;
+
+  if (data) console.log(data);
+
+  return (
+    <div>
+      <h1>USERS</h1>
+      <ul>
+        {data.map(d => (
+            <li key={d.id}>{d.name} {d.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }

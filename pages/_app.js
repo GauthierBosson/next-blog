f3e23c087214f9c4a563def850ae3b28e1f25433
Gauthier Bosson <bosson.gauthier27@gmail.com>
2020-09-24 12:46:46 +0200

    removed amplify for test perf

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,18 @@ import "../styles/globals.css";
 import UserContext from "../lib/context/UserContext";
 
 import { SWRConfig } from "swr";
-import Amplify from "aws-amplify";
+// import Amplify from "aws-amplify";
 import { Provider } from "next-auth/client";
 
-Amplify.configure({
-  Auth: {
-    identityPoolID: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
-    region: process.env.NEXT_PUBLIC_REGION,
-    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
-    userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_WEB_CLIENT_ID,
-  },
-  ssr: true,
-});
+// Amplify.configure({
+//   Auth: {
+//     identityPoolID: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
+//     region: process.env.NEXT_PUBLIC_REGION,
+//     userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+//     userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_WEB_CLIENT_ID,
+//   },
+//   ssr: true,
+// });
 
 function MyApp({ Component, pageProps }) {
   return (
